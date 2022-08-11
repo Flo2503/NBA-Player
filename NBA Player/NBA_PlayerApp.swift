6e98568fe5dbbcf8c225e03f9d54ddd2a4e299d6
Flo2503 <bruel.florent@gmail.com>
2022-08-11 18:02:06 +0200

    Initial Commit

---
+++ NBA Player/NBA_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_PlayerApp.swift
+//  NBA Player
+//
+//  Created by Florent on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBA_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
