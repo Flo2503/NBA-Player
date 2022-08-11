deb43d57b00b88ddf1689a8231053a9a828230bf
Flo2503 <bruel.florent@gmail.com>
2022-08-11 19:21:31 +0200

    Improved player's image

--- NBA Player/ContentView.swift
+++ NBA Player/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("gs").resizable().frame(height: 300)
+        VStack {
+            Image("gs").resizable().aspectRatio(contentMode: .fit)
+            Image("steph").clipShape(Circle()).background(Circle()).foregroundColor(.white).overlay(Circle().stroke(Color.white, lineWidth: 4 )).offset(x:0, y:-90).padding(.bottom, -70).shadow(radius: 15)
+            Text("Steph Curry").font(.system(size: 40)).fontWeight(.heavy)
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
