2f5392bb3a91a6ff1d004bd4b2b7d7fc7d3ff3ce
Flo2503 <bruel.florent@gmail.com>
2022-08-16 19:16:38 +0200

    Created row with player image, background color team and name

--- NBA Player/PlayerRow.swift
+++ NBA Player/PlayerRow.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct PlayerRow: View {
+    
+    let player: Player
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(player.imageName).resizable().aspectRatio(contentMode: .fit).clipShape(Circle()).background(Circle().foregroundColor(player.team.color))
+            Text(player.name).font(.largeTitle)
+            Spacer()
+        }
     }
 }
 
 struct PlayerRow_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerRow()
+        PlayerRow(player: players[0]).previewLayout(.fixed(width: 500, height: 100))
     }
 }
